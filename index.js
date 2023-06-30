ad8e9475a38beab68968ace2d2e9966d2db67430
suraj <sooraz858@gmail.com>
2023-06-30 15:40:03 +0545

    cors origin error

--- index.js
+++ index.js
@@ -39,6 +39,13 @@ mongoose.connect(mongo_uri, {
 })
 
 
+app.use((req, res, next) =>{
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Headers, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+    res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, PATCH, OPTIONS');
+    next();
+})
+
 
 app.use(express.static(path.join(__dirname, './frontend/build')));
 
